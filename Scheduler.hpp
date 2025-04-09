e1f86aac54e89ba358738e186e68be425ec1c0be
Seifalla Ehab <160287704+Seifehab11-8@users.noreply.github.com>
2025-04-09 09:14:43 +0200

    Update Scheduler.hpp

--- Scheduler.hpp
+++ Scheduler.hpp
@@ -5,6 +5,7 @@
 class Scheduler {
     protected:
     std::list<Process> ready_process;
+    std::list<Process> gantt_chart_process;
     public:
     virtual void setProcess(Process &process) = 0;
     virtual std::list<Process>& schedule() = 0;
