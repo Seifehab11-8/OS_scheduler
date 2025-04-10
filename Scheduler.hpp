1c1fa425d9b1aa6029f9b5a83ae8e5b0585fca04
Seifalla Ehab <160287704+Seifehab11-8@users.noreply.github.com>
2025-04-10 11:16:35 +0200

    Add files via upload

--- Scheduler.hpp
+++ Scheduler.hpp
@@ -5,10 +5,9 @@
 class Scheduler {
     protected:
     std::list<Process> ready_process;
-    std::list<Process> gantt_chart_process;
     public:
     virtual void setProcess(Process &process) = 0;
-    virtual std::list<Process> schedule() = 0;
+    virtual std::list<Process>& schedule() = 0;
     Scheduler(std::list<Process>&process);
     Scheduler();
 };
