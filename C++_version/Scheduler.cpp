5f7c62a1fa83502ab3c10eb2519c2966f4f326d7
Seifehab11-8 <semaziz2004@yahoo.com>
2025-04-11 14:23:46 +0200

    update schdeuler

--- C++_version/Scheduler.cpp
+++ C++_version/Scheduler.cpp
@@ -12,22 +12,12 @@ void Scheduler::addProcess(Process &process)
 bool Scheduler::finished()
 {
     return std::all_of(process_queue.begin(), process_queue.end(), [](const Process &a)
-<<<<<<< HEAD
                         {
                             return a.getBurst_time() == a.getExecution_time(); // All processes must have burst_time == 0
                         });
 }
 void Scheduler::printProcesses()
 {
-=======
-                       {
-                           return a.getBurst_time() == a.getExecution_time(); // All processes must have burst_time == execution time of process to be finished
-                       });
-}
-void Scheduler::printProcesses()
-{
-    std::cout << "Processes :\n";
->>>>>>> e1ff5c65ad538945d5afbe6313047a24c1742524
     std::cout << "ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n";
     for (const auto &process : process_queue)
     {
