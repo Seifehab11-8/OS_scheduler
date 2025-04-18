c2addc7954b8337d4a788eeb7ec073490e71ef07
Muhammed Khalid <127145833+muhammedkh45@users.noreply.github.com>
2025-04-18 14:16:36 +0200

    Delete CPUSchedular directory

--- CPUSchedular/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.cpuschedular {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.cpuschedular to javafx.fxml;
-    exports org.example.cpuschedular;
-}
--- CPUSchedular/src/main/java/org/example/cpuschedular/APP.java
+++
@@ -1,8 +0,0 @@
-package org.example.cpuschedular;
-/* this app class has one and only one use */
-/*usage: to link with Meta-inf */
-public class APP {
-    public static void main(String [] args){
-        HelloApplication.main(args);
-    }
-}
